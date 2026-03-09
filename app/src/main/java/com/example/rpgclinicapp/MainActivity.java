296187d49fa6903cfc8b1157a4e3cd81c5e0472b
Aluno Projeto 6 <aluno@exemplo.com>
2026-03-09 19:56:45 -0300

    fix: corrigido incompatibilidade do WindowInsets com RelativeLayout

--- app/src/main/java/com/example/rpgclinicapp/MainActivity.java
+++ app/src/main/java/com/example/rpgclinicapp/MainActivity.java
@@ -1,24 +1,16 @@
 package com.example.rpgclinicapp;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        // Oculta a Action Bar superior
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
